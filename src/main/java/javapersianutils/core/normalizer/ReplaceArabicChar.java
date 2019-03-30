ae23eb561b85571d0afe5ad364a61181ac4a4eda
Razavi <m.razavi777@gmail.com>
2019-03-31 01:04:21 +0430

    replace arabic characters with persian characters

---
+++ src/main/java/javapersianutils/core/normalizer/ReplaceArabicChar.java
@@ -0,0 +1,55 @@
+package javapersianutils.core.normalizer;
+
+import javapersianutils.core.validators.StringUtil;
+
+/**
+ * <h1>ReplaceArabicChar</h1>
+ * The ReplaceArabicChar
+ * <p>
+ *
+ * @author Mahdi Razavi
+ * @version 1.0
+ * @since 3 /31/2019
+ */
+public class ReplaceArabicChar {
+
+    /**
+     * Arabic Ke Char \u0643 = ARABIC LETTER KAF
+     */
+    public static final char ARABIC_KE_CHAR = (char) 1603;
+    /**
+     * Arabic Ye Char \u0649 = ARABIC LETTER ALEF MAKSURA
+     */
+    public static final char ARABIC_YE_Char1 = (char) 1609;
+    /**
+     * Arabic Ye Char \u064A = ARABIC LETTER YEH
+     */
+    public static final char ARABIC_YE_Char2 = (char) 1610;
+    /**
+     * Persian Ke Char \u06A9 = ARABIC LETTER KEHEH
+     */
+    public static final char PERSIAN_KE_CHAR = (char) 1705;
+    /**
+     * Persian Ye Char \u06CC = ARABIC LETTER FARSI YEH
+     */
+    public static final char PERSIAN_YE_CHAR = (char) 1740;
+
+    private ReplaceArabicChar() {
+    }
+
+    /**
+     * Fixes common writing mistakes caused by using a bad keyboard layout,
+     * such as replacing Arabic Ye with Persian one and so on ...
+     *
+     * @param data the data
+     * @return the string
+     */
+    public static String replaceArabicChar(String data) {
+        return StringUtil.isNullOrWhiteSpace(data) ?
+                "" :
+                data.replace(ARABIC_YE_Char1, PERSIAN_YE_CHAR)
+                        .replace(ARABIC_YE_Char2, PERSIAN_YE_CHAR)
+                        .replace(ARABIC_KE_CHAR, PERSIAN_KE_CHAR)
+                        .trim();
+    }
+}
---
+++ src/test/java/javapersianutils/core/normalizer/ReplaceArabicCharTest.java
@@ -0,0 +1,16 @@
+package javapersianutils.core.normalizer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReplaceArabicCharTest {
+
+    @Test
+    public void Test_initializeIntervalClass_fail() {
+        String textWithArabicChars = "کی";
+        String textWithPersianChars = "کی";
+        assertEquals(textWithPersianChars, ReplaceArabicChar.replaceArabicChar(textWithArabicChars));
+    }
+
+}
